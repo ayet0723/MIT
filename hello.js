5b1a141b574412dbb64863d48044269646ec88ca
AZrepo <azimat0628@gmail.com>
2024-08-04 20:49:43 -0400

    Made the greeting message more fun

--- hello.js
+++ hello.js
@@ -1,4 +1,20 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const greetings = [
+    "Hello World from the Office Hours in the terminal!",
+    "Greetings from the coding world!",
+    "Hi there! Office Hours are in session!",
+    "Hey! Ready to learn some cool stuff?",
+    "Hello, curious coder! Welcome to the Office Hours!"
+];
+
+const getRandomGreeting = () => greetings[Math.floor(Math.random() * greetings.length)];
+
+const asciiBanner = `
+    ____  _  _  ____  __    ___  _  _    _  _  __  __  ____  ____ 
+   / ___)/ )( \\(  __)(  )  / __)( \\/ )  / )( \\(  )(  )(  __)/ ___)
+  / (___) /\\ / ) _) / (_/\\\\__ \\/ \\/ \\  ) \\/ ( )(__)(  ) _) \\___ \\
+   \\____)\\__/ (____)\\____/(____/\\_)(_/  \\____/\\____/ (____)(____/
+  `;
+
+const hello = () => `${asciiBanner}\n\n${getRandomGreeting()}`;
 console.log(hello());
 exports.hello = hello;
-
